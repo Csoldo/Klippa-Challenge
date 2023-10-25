c3da62d58ad3b6a18901ddbe010c9861513810c2
Csoldo <nagyboldi2000@gmail.com>
2023-10-25 18:11:54 +0200

    refactor, add folder monitoring and config file option

--- klippa/api.py
+++ klippa/api.py
@@ -1,109 +1,145 @@
 import requests
 import json
 import os
-import threading
+import multiprocessing
 from rich import print
 
-def scan(api_key: str, template: str, file_path: str, fast: bool = False, save_json: str = None):
-    """
-    Scan the contents of a folder (concurrently) or a single file
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
-    Parameters
-    ----------
-    api_key : str
-        API key for authentication
-    template : str
-        Template for OCR processing
-    file_path : str
-        Path to the PDF or image file (or directory of files) to be processed
-    fast : bool, optional
-        Use fast text extraction, otherwise mode is full, by default False
-    save_json : str, optional
-        Save the JSON response to a file. Note: do not provide extension. Ex: output, by default None
-    """
-    if os.path.isdir(file_path):
-        # If file_path is a directory, scan all files in the directory concurrently (or subdirectories)
-        threads = []
-        for filename in os.listdir(file_path):
-            file_full_path = os.path.join(file_path, filename)
-            thread = threading.Thread(target=scan, args=(api_key, template, file_full_path, fast, save_json))
-            thread.start()
-            threads.append(thread)
+class KlippaAPI:
+    def __init__(self, api_key: str, template: str, file_path: str, fast: bool = False, save_json: str = None):
+        self.api_key = api_key
+        self.template = template
+        self.file_path = file_path
+        self.fast = fast
+        self.save_json = save_json
+        self.scanned_documents = []
 
-        for thread in threads:
-            thread.join()
+    def scan(self):
+        """
+        Scan the contents of a folder (concurrently) or a single file
+        """
+        if os.path.isdir(self.file_path):
+            # If file_path is a directory, scan all files in the directory concurrently (or subdirectories)
+            filenames = []
+            for filename in os.listdir(self.file_path):
+                file_full_path = os.path.join(self.file_path, filename)
+                filenames.append(file_full_path)
+        
+            with multiprocessing.Pool() as pool:
+                pool.map(self.scan_document, filenames)
 
-    elif os.path.isfile(file_path):
-        # If file_path is a single file, scan it
-        scan_document(api_key, template, file_path, fast, save_json)
-    else:
-        print(f"Invalid file path: {file_path}")
+            self.monitor_folder()
 
-
-
-def scan_document(api_key: str, template: str, file_path: str, fast: bool = False, save_json: str = None):
-    """
-    Scan a file and call the appropriate API requests
-
-    Parameters
-    ----------
-    api_key : str
-        API key for authentication
-    template : str
-        Template for OCR processing
-    file_path : str
-        Path to the PDF or image file (or directory of files) to be processed
-    fast : bool, optional
-        Use fast text extraction, otherwise mode is full, by default False
-    save_json : str, optional
-        Save the JSON response to a file. Note: do not provide extension. Ex: output, by default None
-    """
-    # Parameters for the request
-    base_url = "https://custom-ocr.klippa.com/api/v1"
-    extraction_mode = "fast" if fast else "full" 
-    url = f"{base_url}/parseDocument/{template}"
-            
-    # Prepare request headers
-    headers = {"X-Auth-Key": api_key}
-    files = {"document": open(file_path, "rb")}
-    data = {"pdf_text_extraction": extraction_mode}
-
-    try:
-        response = requests.post(url=url, headers=headers, files=files, data=data)
-    except requests.exceptions.ConnectionError as e:
-        print("error", str(e))
-    
-    if response.status_code == 200:
-        data = response.content.decode()
-        dict_data = json.loads(data)
-
-        if save_json:
-            json_object = json.dumps(dict_data, indent=4)
-            output_path = sanitize_file_path(save_json)
-
-            with open(output_path, "w") as outfile:
-                outfile.write(json_object)
+        elif os.path.isfile(self.file_path):
+            # If file_path is a single file, scan it
+            self.scan_document(self.file_path)
         else:
-            print(dict_data)
-    else:
-        error_message = f"Error: {response.status_code} - {response.reason}"
-        print(error_message)
+            print(f"Invalid file path: {self.file_path}")
 
 
-def sanitize_file_path(file_path: str) -> str:
-    """
-    Sanitize the file path to avoid overwriting existing files
-    """
-    file_path_sanitized = file_path
-    if not file_path.endswith(".json"):
-        file_path_sanitized = f"{file_path}.json"
+    def scan_document(self, single_file_path: str):
+        """
+        Scan a file and call the appropriate API requests
+        """
+        # Parameters for the request
+        base_url = "https://custom-ocr.klippa.com/api/v1"
+        extraction_mode = "fast" if self.fast else "full" 
+        url = f"{base_url}/parseDocument/{self.template}"
 
-    if os.path.exists(file_path_sanitized):
-        cnt = 1
-        tmp = file_path_sanitized.split(".json")[0]
-        while os.path.exists(f"{tmp}({cnt}).json"):
-            cnt += 1
+        # Prepare request headers
+        headers = {"X-Auth-Key": self.api_key}
+        files = {"document": open(single_file_path, "rb")}
+        data = {"pdf_text_extraction": extraction_mode}
 
-        file_path_sanitized = f"{tmp}({cnt}).json"
-    return file_path_sanitized
+        try:
+            response = requests.post(url=url, headers=headers, files=files, data=data)
+        except requests.exceptions.ConnectionError as e:
+            print("error", str(e))
+        
+        if response.status_code == 200:
+            data = response.content.decode()
+            dict_data = json.loads(data)
+
+            if self.save_json:
+                json_object = json.dumps(dict_data, indent=4)
+                output_path = self.sanitize_file_path(self.save_json)
+
+                with open(output_path, "w") as outfile:
+                    outfile.write(json_object)
+            else:
+                print(dict_data)
+
+            self.scanned_documents.append(dict_data)
+            self.print_folder_totals()
+        else:
+            error_message = f"Error: {response.status_code} - {response.reason}"
+            print(error_message)
+
+
+    def sanitize_file_path(self, single_file_path: str) -> str:
+        """
+        Sanitize the file path to avoid overwriting existing files
+        """
+        file_path_sanitized = single_file_path
+        if not single_file_path.endswith(".json"):
+            file_path_sanitized = f"{single_file_path}.json"
+
+        if os.path.exists(file_path_sanitized):
+            cnt = 1
+            tmp = file_path_sanitized.split(".json")[0]
+            while os.path.exists(f"{tmp}({cnt}).json"):
+                cnt += 1
+
+            file_path_sanitized = f"{tmp}({cnt}).json"
+        return file_path_sanitized
+        
+    def monitor_folder(self):
+        """
+        Monitor a folder for new files and scan them
+        """
+
+        event_handler = Handler(api=self)
+        observer = Observer()
+        observer.schedule(event_handler, path=self.file_path, recursive=True)
+        observer.start()
+        try:
+            while True:
+                pass
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
+
+    def print_folder_totals(self):
+        total_amount = 0
+        total_vat = 0
+        for doc in self.scanned_documents:
+            if not "data" in doc:
+                continue
+            if not doc["data"]["parsed"]:
+                continue
+            if not "amount" in doc["data"]["parsed"] or not "vatamount" in doc["data"]["parsed"]:
+                continue
+            total_amount += doc["data"]["parsed"]["amount"]
+            total_vat += doc["data"]["parsed"]["vatamount"]
+        if total_amount == 0:
+            return
+        print(f"Total amount: {total_amount}")
+        print(f"Total VAT: {total_vat}")
+        print(f"Vat percentage: {total_vat / total_amount * 100}")
+
+
+
+class Handler(FileSystemEventHandler):
     
+    def __init__(self, api: KlippaAPI):
+        self.api = api
+
+    def on_created(self, event):
+        if event.is_directory:
+            return None
+ 
+        print("New file added to folder- % s." % event.src_path)
+        print("Scanning file...")
+        self.api.scan_document(event.src_path)
--- klippa/cli.py
+++ klippa/cli.py
@@ -1,7 +1,16 @@
 import typer
-from api import scan
+from api import KlippaAPI
+import json
 
 app = typer.Typer()
+
+# Load configuration values from config.json
+config = {}
+try:
+    with open("config.json", "r") as config_file:
+        config = json.load(config_file)
+except FileNotFoundError:
+    config = {}
 
 @app.command("hello")
 def hello(name: str):
@@ -9,15 +18,25 @@ def hello(name: str):
 
 @app.command("scan")
 def apiCall(
-    api_key: str = typer.Option(None, prompt=True, help="(Required) API key for authentication"),
-    template: str = typer.Option(None, prompt=True, help="(Required) Template for OCR processing"),
+    api_key: str = typer.Option(config.get("api_key"), prompt="API Key", help="(Required) API key for authentication"),
+    template: str = typer.Option(config.get("template"), prompt="Template", help="(Required) Template for OCR processing"),
     file_path: str = typer.Option(None, prompt=True, help="(Required) Path to the PDF or image file (or directory of files) to be processed)"),
-    fast: bool = typer.Option(True, help="Use fast text extraction"),
+    fast: bool = typer.Option(config.get("fast"), help="Use fast text extraction"),
     save_json: str = typer.Option(None, help="Save the JSON response to a file. Note: do not provide extension. Ex: output"),
     ):
     """Scan a PDF or image file (or directory of files) using the Klippa API"""
 
-    scan(api_key, template, file_path, fast, save_json)
+    # Update the config.json file with the provided values
+    config["api_key"] = api_key
+    config["template"] = template
+    config["fast"] = fast
+
+    with open("config.json", "w") as config_file:
+        json.dump(config, config_file, indent=4)
+
+    api = KlippaAPI(api_key, template, file_path, fast, save_json)
+    api.scan()
+
 
 
 if __name__ == "__main__":
